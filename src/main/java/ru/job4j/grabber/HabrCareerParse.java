107b8660bd7979228a8428ea70348aee9077659a
geraldik <geraldik1986@gmail.com>
2022-04-02 20:55:49 +0300

    Update

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -24,20 +24,25 @@ public class HabrCareerParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         var habrCareerParse = new HabrCareerParse(new HarbCareerDateTimeParser());
         List<Post> posts = habrCareerParse.list(PAGE_LINK);
         posts.forEach(System.out::println);
     }
 
-    private static String retrieveDescription(String link) throws IOException {
+    private static String retrieveDescription(String link) {
         Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
+        Document document = null;
+        try {
+            document = connection.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Elements description = document.getElementsByClass("style-ugc");
         return description.text();
     }
 
-    private Post getPost(Element row) throws IOException {
+    private Post getPost(Element row) {
         Element titleElement = row.select(".vacancy-card__title").first();
         Element linkElement = titleElement.child(0);
         Element timeElement = row.select(".vacancy-card__date").first().child(0);
@@ -60,13 +65,7 @@ public class HabrCareerParse implements Parse {
                 e.printStackTrace();
             }
             Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                try {
-                    posts.add(getPost(row));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+            rows.forEach(row -> posts.add(getPost(row)));
         }
 
         return posts;
